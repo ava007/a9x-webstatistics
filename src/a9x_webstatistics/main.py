1f5a83e74feb557d2358645d3b23bc4c1075297f
ava007 <avonarx@gmx.net>
2024-03-30 17:51:37 +0100

    Create main.py

---
+++ src/a9x_webstatistics/main.py
@@ -0,0 +1,18 @@
+import io, sys, argparse, json, ast
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--statfile", help="json file that contains calculated values", default="webstat.json")
+    parser.add_argument("-i", "--infile", help="filename including path to web server access log that contains input data", default="/var/log/nginx-access-logikfabrik.json")
+    args = parser.parse_args()
+
+    if args.cmd == "crtstats":
+        parser.print_help()
+    elif args.cmd == "crtissues":
+        parser.print_help()
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
