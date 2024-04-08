0cab19ca924432a64f173f5a572fb76f5c73d1a9
ava007 <avonarx@gmx.net>
2024-04-08 20:43:46 +0200

    Update test_module1.py

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -6,7 +6,7 @@ from a9x_webstatistics.module1 import Number
 class TestSimple(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual((Number(5) + Number(6)).value, 11)
+        self.assertEqual((Number(7) + Number(6)).value, 13)
 
 
 if __name__ == '__main__':
