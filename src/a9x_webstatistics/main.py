b4091d5d9cd27b1d4766d0d28f6b0204dc301ff4
ava007 <avonarx@gmx.net>
2024-04-08 21:23:31 +0200

    Update main.py

--- src/a9x_webstatistics/main.py
+++ src/a9x_webstatistics/main.py
@@ -2,8 +2,8 @@ import io, sys, argparse, json, ast
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--statfile", help="json file that contains calculated values", default="webstat.json")
-    parser.add_argument("-i", "--infile", help="filename including path to web server access log that contains input data", default="/var/log/nginx-access-logikfabrik.json")
+    parser.add_argument("-s", "--statfile", help="json file that contains calculated statistics", default="webstat.json")
+    parser.add_argument("-i", "--infile", help="filename including path to web server access log that contains input data", default="/var/log/nginx-access.json")
     args = parser.parse_args()
 
     if args.cmd == "crtstats":
