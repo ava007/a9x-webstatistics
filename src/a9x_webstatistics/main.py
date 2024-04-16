edfa3e4f5b717bb080e59167b4b22a982ae9250a
ava007 <avonarx@gmx.net>
2024-04-16 21:46:46 +0200

    Update main.py

--- src/a9x_webstatistics/main.py
+++ src/a9x_webstatistics/main.py
@@ -1,5 +1,24 @@
 import io, sys, argparse, json, ast
 
+def detectDeviceClass(ua):
+  if ua is None:
+     return 'desktop'
+  if (ua.lower().find('bot') > 0):
+     return 'bots'
+  if (ua.lower().find('python') > 0):
+     return 'bots'
+  if (ua.lower().find('mediapartner') > 0):
+     return 'others'
+  if (ua.lower().find('curl') > 0):
+     return 'others'
+  if (ua.lower().find('ipad') > 0):
+     return 'tablet'
+  if (ua.lower().find('mobile') > 0):
+     return 'mobile'
+  if (ua.lower().find('android') > 0):
+     return 'tablet'
+  return 'desktop'
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--statfile", help="json file that contains calculated statistics", default="webstat.json")
@@ -12,6 +31,21 @@ def main():
         parser.print_help()
     else:
         parser.print_help()
+
+    # init statistic file:
+    d = {}
+    d['timelastrec'] = '19991231235959'
+    d['days'] = {}
+
+    # load statistic file if it exists
+    try:
+        f = open(args.statfile) 
+        d = json.load(f) 
+    except FileNotFoundError:   # first call: file does not exists
+        print("-s Statisticfile not found, it will be created")
+    except json.JSONDecodeError:
+        print("-s Json File not valid")
+
 
 
 if __name__ == "__main__":
