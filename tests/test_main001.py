c98fcd1dd14d414d96610b2f0753f837b737f6b1
ava007 <avonarx@gmx.net>
2024-04-17 18:07:02 +0200

    Update test_main001.py

--- tests/test_main001.py
+++ tests/test_main001.py
@@ -7,13 +7,11 @@ import a9x_webstatistics.main
 class TestMain(unittest.TestCase):
 
     def test_main(self):
-        file = Path("webstat.json")  
         print("home: " + str(Path.home()) )
+        file = Path("webstat.json")  
         with open(file) as f:  
             input_data = f.read()  
-        #expected = "Jerry was born in 1968"  
         assert timelastrec(input_data) == '19991231235959'
-        #self.assertEqual((Number(7) + Number(6)).value, 13)
 
 if __name__ == '__main__':
     unittest.main()
