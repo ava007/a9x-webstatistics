fbefba6a761710045cea00fbbf1a9b13189a703c
ava007 <avonarx@gmx.net>
2024-04-17 15:19:34 +0200

    Update test_main001.py

--- tests/test_main001.py
+++ tests/test_main001.py
@@ -1,7 +1,13 @@
-import sys
 import unittest
 
-import pytest
+import a9x_webstatistics.main
 
-if __name__ == "__main__":
-    sys.exit(pytest.main(["-qq"]))
+
+class TestMain(unittest.TestCase):
+
+    def test_main(self):
+        #self.assertEqual((Number(7) + Number(6)).value, 13)
+
+
+if __name__ == '__main__':
+    unittest.main()
