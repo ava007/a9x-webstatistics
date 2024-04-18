70d69e9bc83669ab29f2b4c36fb08814a2ca3b05
ava007 <avonarx@gmx.net>
2024-04-18 14:32:18 +0200

    Create test_main010.py

---
+++ tests/test_main010.py
@@ -0,0 +1,21 @@
+import unittest
+from pathlib import Path
+
+from a9x_webstatistics.main import *
+
+class TestMain010(unittest.TestCase):
+
+    def test_main010(self):
+        print("home: " + str(Path.home()) )
+        print(str(list(path.iterdir())))
+        # calling runws expecting return 0
+        #assert runws() == 0
+        #file = Path("webstat.json")  
+        #with open(file) as f:  
+        #    file_data = f.read()  
+        #print(str(file_data))
+        #contents = json.loads(file_data)
+        #assert '19991231235959' in contents['timelastrec']
+
+if __name__ == '__main__':
+    unittest.main()
