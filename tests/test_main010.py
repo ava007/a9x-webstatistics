7b521f2f1205c860f5b58e914b28cdb6d2d63ad9
ava007 <avonarx@gmx.net>
2024-04-19 08:24:43 +0200

    Update test_main010.py

--- tests/test_main010.py
+++ tests/test_main010.py
@@ -9,6 +9,19 @@ class TestMain010(unittest.TestCase):
         print("home: " + str(Path.home()) )
         p = Path('.')
         [x for x in p.iterdir() if x.is_dir()]
+
+        # copy input test file 
+        source = 'test_access.log'
+        destination = 'nginx_access.log'
+        with open(source, 'rb') as file:
+            tiFile = file.read()
+        with open(destination, 'wb') as toFile:
+            toFile.write(tiFile)
+
+        p = Path('.').glob('**/*')
+        files = [x for x in p if x.is_file()]
+        print(str(files))
+        
         # calling runws expecting return 0
         #assert runws() == 0
         #file = Path("webstat.json")  
