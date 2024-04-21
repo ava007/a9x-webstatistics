dcd85c00a67536af846257a292df4d8f7c7cf2df
ava007 <avonarx@gmx.net>
2024-04-21 12:01:05 +0200

    Update updatestatistics.py

--- src/a9x_webstatistics/updatestatistics.py
+++ src/a9x_webstatistics/updatestatistics.py
@@ -1,6 +1,7 @@
 def upd(
     d,
-    i):
+    i
+    visitIP):
         
     dt = i['dt'].strftime("%Y%m%d")
 
@@ -8,12 +9,16 @@ def upd(
     if dt not in d['days']:
         d['days'][dt] = {}
         d['days'][dt]['hits']    = 0;
-        d['days'][dt]['visits']  = 1;
+        d['days'][dt]['visits']  = 0;
         d['days'][dt]['size']    = 0;
         d['days'][dt]['serverResponseCode'] = {}
 
     # update statistics
     d['days'][dt]['hits'] = d['days'][dt]['hits'] + 1
     d['timelastrec'] = i['dt'].strftime("%Y%m%d%H%M%S")
+
+    if i['ip'] not in visitIP:
+        d['days'][dt]['visits'] = d['days'][dt]['visits'] + 1;
+        visitIP[i['ip']] = 1
         
     return d
