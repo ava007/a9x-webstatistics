dd33ade846f93520f09fae2bc1eb8ad5aa046bab
ava007 <avonarx@gmx.net>
2024-04-21 17:42:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     # install_requires=['Pillow'],
     extras_require={
         'dev': ['check-manifest'],
