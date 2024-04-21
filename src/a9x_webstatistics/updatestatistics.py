7a8f41089d66bf7cacf5821c0ba1a6dfd92370a0
ava007 <avonarx@gmx.net>
2024-04-21 09:23:19 +0200

    Update updatestatistics.py

--- src/a9x_webstatistics/updatestatistics.py
+++ src/a9x_webstatistics/updatestatistics.py
@@ -1,2 +1,18 @@
-def upd():
-    print("Hello")
+def upd(
+    d,
+    i):
+        
+    dt = i['dt'].strftime("%Y%m%d")
+
+    # init a new day with minimal attributes:
+    if dt not in d['days']:
+        d['days'][dt] = {}
+        d['days'][dt]['hits']    = 0;
+        d['days'][dt]['visits']  = 1;
+        d['days'][dt]['size']    = 0;
+        d['days'][dt]['serverResponseCode'] = {}
+
+    # update statistics
+    d['days'][dt]['hits'] = d['days'][dt]['hits'] + 1
+        
+    return d
