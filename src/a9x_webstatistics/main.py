7e05d82e30ba9970de806d532ab65d0a813c5867
ava007 <avonarx@gmx.net>
2024-04-25 20:47:12 +0200

    Update main.py

--- src/a9x_webstatistics/main.py
+++ src/a9x_webstatistics/main.py
@@ -24,11 +24,12 @@ def parseRec(rec, log_pattern):
         referer = match.group(6)
         user_agent = match.group(7)
 
-        dt = datetime.strptime(timestamp,'%d/%b/%Y:%H:%M:%S %z'),  # 07/Jan/2024:14:06:24 +0000
+        dto = datetime.strptime(timestamp,'%d/%b/%Y:%H:%M:%S %z'),  # 07/Jan/2024:14:06:24 +0000
                 
         r = {
             'ip': ip_address,
-            'timestamp': datetime.strptime(dt,"%Y%m%d%H%M%S"),
+            'ymd': dto.strftime("%Y%m%d"),
+            'timestamp': dto.strftime("%Y%m%d%H%M%S") ,
             'request': request,
             'status_code': status_code,
             'bytes_sent': bytes_sent,
