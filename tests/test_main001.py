f2d12d7962c6c1c99bdd8b1cb71613072667710e
ava007 <avonarx@gmx.net>
2024-05-03 20:12:43 +0200

    Update test_main001.py

--- tests/test_main001.py
+++ tests/test_main001.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from a9x_webstatistics.main import *
 from a9x_webstatistics.updatestatistics import upd
+from subprocess import run
 
 class TestMain(unittest.TestCase):
 
@@ -16,6 +17,10 @@ class TestMain(unittest.TestCase):
         print(str(file_data))
         contents = json.loads(file_data)
         assert '20240130144922' in contents['timelastrec']
+
+        cmddata = run("ls -altr", capture_output=True, shell=True, text=True)
+        print(cmddata.stdout) 
+        print(cmddata.stderr) 
 
 if __name__ == '__main__':
     unittest.main()
