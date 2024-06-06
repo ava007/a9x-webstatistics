2a278ef6f889ef35aef3fff3ccc631969b7a3821
ava007 <avonarx@gmx.net>
2024-06-06 21:04:18 +0200

    Update gencockpit.py

--- src/a9x_webstatistics/gencockpit.py
+++ src/a9x_webstatistics/gencockpit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import io, sys, argparse, json, ast
-from operator import itemgetter
+from operator import itemgetter, iteritems
 from copy import deepcopy
 from importlib.metadata import version
 from datetime import datetime
