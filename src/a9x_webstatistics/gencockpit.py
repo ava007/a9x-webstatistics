b294a404df47dbebd651f9f851f5d812aa15f8e8
ava007 <avonarx@gmx.net>
2024-06-06 19:59:14 +0200

    Update gencockpit.py

--- src/a9x_webstatistics/gencockpit.py
+++ src/a9x_webstatistics/gencockpit.py
@@ -82,7 +82,7 @@ def runGenCockpit(infile, outfile):
         h += '<p><pre>'
         for k, v in reversed(d['days'].items()):
             # k = 20240228 v = array
-            if source in v:
+            if 'source' in v:
                 for ks, vs in v['source']:
                     h += ks + " " + vs + "\n"
         h += '</pre></p>' + "\n"
