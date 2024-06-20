855491177de61561e82594e891a36bff628bea30
ava007 <3790170+ava007@users.noreply.github.com>
2024-06-20 19:45:30 +0200

    Update test_z090_gencockpit010.py

--- tests/test_z090_gencockpit010.py
+++ tests/test_z090_gencockpit010.py
@@ -12,7 +12,7 @@ class TestGencockpit010(unittest.TestCase):
         print(cmddata.stdout) 
         
         # calling runws expecting return 0
-        assert runGenCockpit(infile="webstat.json",outfile="webstat.html") == 0
+        assert runGenCockpit(infile="webstat.json",outfile="webstat.html",domain="https://logikfabrik.com") == 0
         with open('webstat.html') as f:  
             file_data = f.read()  
         print(str(file_data))
