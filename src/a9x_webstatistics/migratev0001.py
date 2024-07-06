e12c8475cf505d60ef34fc7beb64bc54ad7835b7
ava007 <3790170+ava007@users.noreply.github.com>
2024-07-06 20:57:21 +0200

    Update migratev0001.py

--- src/a9x_webstatistics/migratev0001.py
+++ src/a9x_webstatistics/migratev0001.py
@@ -1,3 +1,20 @@
 def migv0001(
     d,
     ):
+    # return if already migrated:
+    if 'v0001' in d:
+        return
+    d['v0001'] = {}
+    d['v0001']['days'] = {}
+    for x in d['days']:
+        d['v0001']['days'][x] = {}
+        d['v0001']['days'][x]['user'] = {}
+        d['v0001']['days'][x]['robots'] = {}
+        d['v0001']['days'][x]['quality'] = {}
+
+    # write updated statistic file:
+    with open('webstatv0001.json', "w") as sf:
+       json.dump(d,sf)  
+
+        
+
