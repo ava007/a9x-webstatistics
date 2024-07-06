2ba5026458282f3798befc2656ba2f0544b2cc39
ava007 <3790170+ava007@users.noreply.github.com>
2024-07-06 21:42:02 +0200

    Update migratev0001.py

--- src/a9x_webstatistics/migratev0001.py
+++ src/a9x_webstatistics/migratev0001.py
@@ -28,7 +28,7 @@ def migv0001(
         d['v0001']['days'][x]['robot'] = 0
         if 'bots' in d['days'][x]['device_hits']:
             d['v0001']['days'][x]['robot'] += d['days'][x]['device_hits']['bots'] 
-        if 'others' in d['days']['device_hits']:
+        if 'others' in d['days'][x]['device_hits']:
             d['v0001']['days'][x]['robot'] += d['days'][x]['device_hits']['others'] 
         d['v0001']['days'][x]['quality'] = {}
 
