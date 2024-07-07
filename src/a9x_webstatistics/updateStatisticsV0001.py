22798ba9f4fab6ba26bcfff84b8170be852318f0
ava007 <3790170+ava007@users.noreply.github.com>
2024-07-07 08:11:24 +0200

    Update updateStatisticsV0001.py

--- src/a9x_webstatistics/updateStatisticsV0001.py
+++ src/a9x_webstatistics/updateStatisticsV0001.py
@@ -16,79 +16,85 @@ def updV0001(
     # init a new day with minimal attributes:
     if dt not in d['days']:
         d['v0001']['days'][dt] = {}
-        d['v0001']['days'][dt]['hits']    = 0;
-        d['v0001']['days'][dt]['visits']  = 0;
-        d['v0001']['days'][dt]['bytes_sent'] = 0;
-        d['v0001']['days'][dt]['serverResponseCode'] = {}
-        d['v0001']['days'][dt]['device_hits'] = {} 
-        d['v0001']['days'][dt]['device_hits']['mobile']  = 0 
-        d['v0001']['days'][dt]['device_hits']['tablet']  = 0 
-        d['v0001']['days'][dt]['device_hits']['desktop'] = 0 
-        d['v0001']['days'][dt]['device_hits']['bots']    = 0 
-        d['v0001']['days'][dt]['device_hits']['others']  = 0 
+        d['v0001']['days'][dt]['user']    = {};
+        d['v0001']['days'][dt]['user']['visits']  = 0;
+        d['v0001']['days'][dt]['user']['bytesSent'] = 0;
 
-    # update statistics
-    d['v0001']['days'][dt]['hits'] = d['days'][dt]['hits'] + 1
-    d['v0001']['days'][dt]['bytes_sent'] = str(int(d['days'][dt]['bytes_sent']) + int(i['bytes_sent']))
+        d['v0001']['days'][dt]['user']['deviceHits'] = {}
+        d['v0001']['days'][dt]['user']['deviceHits']['mobile']  = 0 
+        d['v0001']['days'][dt]['user']['deviceHits']['tablet']  = 0 
+        d['v0001']['days'][dt]['user']['deviceHits']['desktop'] = 0 
+        d['v0001']['days'][dt]['user']['serverResponseCode'] = {}
 
-    if i['status'] in d['v0001']['days'][dt]['serverResponseCode']:
-        d['v0001']['days'][dt]['serverResponseCode'][i['status']] += 1
-    else:
-        d['v0001']['days'][dt]['serverResponseCode'][i['status']] = 1
+        d['v0001']['days'][dt]['robot']    = {};
+        d['v0001']['days'][dt]['robot']['robotHits']  = 0;
+        d['v0001']['days'][dt]['robot']['bytesSent'] = 0;
+        d['v0001']['days'][dt]['robot']['serverResponseCode'] = {}
+        d['v0001']['days'][dt]['quality']    = {};
 
-    # process country if available:
-    if 'country' in i:
-        if 'countries' not in d['days'][dt]:
-            d['v0001']['days'][dt]['countries'] = {}
-        if i['country'] not in d['v0001']['days'][dt]['countries']:
-            d['v0001']['days'][dt]['countries'][i['country']] = 0;
-        d['v0001']['days'][dt]['countries'][i['country']] = d['v0001']['days'][dt]['countries'][i['country']] + 1;
-
-    # update hits for device types:
     devCla = detectDeviceClass(i['user_agent']) 
-    if devCla not in d['v0001']['days'][dt]['device_hits']:
-        d['v0001']['days'][dt]['device_hits'][devCla] = 0 
-    d['v0001']['days'][dt]['device_hits'][devCla] = d['v0001']['days'][dt]['device_hits'][devCla] + 1 
+    # update statistics for user:
+    if devCla in ('desktop','mobile','tablet'):
+        #d['v0001']['days'][dt]['hits'] = d['days'][dt]['hits'] + 1
+        d['v0001']['days'][dt]['user']['bytesSent'] += int(i['bytes_sent'])
 
-    # update external source:
-    if 'referer' in i and i['referer'] != "-":
-        if 'source' not in d['v0001']['days'][dt]:
-            d['v0001']['days'][dt]['source'] = {}
-        refurl = urlparse(i['referer']).netloc
-        rdomain = refurl.removeprefix('www.')
-        if rdomain not in d['days'][dt]['source']:
-            d['v0001']['days'][dt]['source'][rdomain] = 0;
-        d['v0001']['days'][dt]['source'][rdomain] += 1;
+        if i['status'] in d['v0001']['days'][dt]['user']['serverResponseCode']:
+            d['v0001']['days'][dt]['user']['serverResponseCode'][i['status']] += 1
+        else:
+            d['v0001']['days'][dt]['user']['serverResponseCode'][i['status']] = 1
+
+        # process country if available:
+        if 'country' in i:
+            if 'countryHits' not in d['v0001']['days'][dt]['user']:
+                d['v0001']['days'][dt]['user']['countryHits'] = {}
+            if i['country'] not in d['v0001']['days'][dt]['user']['countryHits']:
+                d['v0001']['days'][dt]['user']['countryHits'][i['country']] = 0;
+            d['v0001']['days'][dt]['user']['countryHits'][i['country']] += 1;
+
+        # update hits for device types:
+           if devCla not in d['v0001']['days'][dt]['user']['deviceHits']:
+            d['v0001']['days'][dt]['user']['deviceHits'][devCla] = 0 
+        d['v0001']['days'][dt]['user']['deviceHits'][devCla] += 1 
+
+        # update external source:
+        #if 'referer' in i and i['referer'] != "-":
+        #    if 'externalFriendsHits' not in d['v0001']['days'][dt]['user']:
+        #        d['v0001']['days'][dt]['user']['externalFriendsHits'] = {}
+        #    refurl = urlparse(i['referer']).netloc
+        #    rdomain = refurl.removeprefix('www.')
+        #    if rdomain not in d['days'][dt]['user']['externalFriendsHits']:
+        #        d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain] = 0;
+        #    d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain] += 1;
                     
-    d['timelastrec'] = i['timestamp']
+        d['timelastrec'] = i['timestamp']
 
-    # update visits
-    if i['ip'] not in visitIP and devCla in ('desktop','mobile','tablet'):
-        d['v0001']['days'][dt]['visits'] = d['v0001']['days'][dt]['visits'] + 1;
-        visitIP[i['ip']] = 1
+        # update visits
+        if i['ip'] not in visitIP :
+            d['v0001']['days'][dt]['user']['visits'] = d['v0001']['days'][dt]['user']['visits'] + 1;
+            visitIP[i['ip']] = 1
 
-    # top urls: accumulate top urls on the first day of the month
-    dtTopUrl = dt[0:6] + '01'
-    if i['status'] == '200' and devCla in ('desktop','mobile','tablet'):
-        if 'topurl' not in d['v0001']['days'][dtTopUrl]:
-            d['v0001']['days'][dtTopUrl]['topurl'] = {}
-        if i['request'] not in d['v0001']['days'][dtTopUrl]['topurl']:
-            d['v0001']['days'][dtTopUrl]['topurl'][i['request']] = 0;
-        d['v0001']['days'][dtTopUrl]['topurl'][i['request']] += 1;
+        # top urls: accumulate top urls on the first day of the month
+        dtTopUrl = dt[0:6] + '01'
+        if i['status'] == '200':
+            if 'topUrl' not in d['v0001']['days'][dtTopUrl]['user']:
+                d['v0001']['days'][dtTopUrl]['user']['topUrl'] = {}
+            if i['request'] not in d['v0001']['days'][dtTopUrl]['user']['topUrl']:
+                d['v0001']['days'][dtTopUrl]['user']['topUrl'][i['request']] = 0;
+            d['v0001']['days'][dtTopUrl]['user']['topUrl'][i['request']] += 1;
 
-    # update friends on the first day of the month:
-    dtFriends = dt[0:6] + '01'
-    if i['status'] == '200' and devCla in ('desktop','mobile','tablet') and len(i['referer']) > 1 and i['referer'][0:4] == 'http' and domain not in i['referer']:
-        refurl = urlparse(i['referer']).netloc
-        rdomain = refurl.removeprefix('www.')
-        if 'friends' not in d['v0001']['days'][dtFriends]:
-            d['v0001']['days'][dtFriends]['friends'] = {}
-        if rdomain not in d['v0001']['days'][dtFriends]['friends']:
-            d['v0001']['days'][dtFriends]['friends'][rdomain] = {'cnt': 0, 'target': {} };
-        if i['request'] not in d['v0001']['days'][dtFriends]['friends'][rdomain]['target']:
-            d['v0001']['days'][dtFriends]['friends'][rdomain]['target'][i['request']] = 0
-        d['v0001']['days'][dtFriends]['friends'][rdomain]['target'][i['request']] += 1
-        d['v0001']['days'][dtFriends]['friends'][rdomain]['cnt'] += 1
+        # update friends on the first day of the month:
+        dtFriends = dt[0:6] + '01'
+        if i['status'] == '200' and len(i['referer']) > 1 and i['referer'][0:4] == 'http' and domain not in i['referer']:
+            refurl = urlparse(i['referer']).netloc
+            rdomain = refurl.removeprefix('www.')
+            if 'externalFriendsHits' not in d['v0001']['days'][dtFriends]['user']:
+                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'] = {}
+            if rdomain not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits']:
+                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain] = {'cnt': 0, 'target': {} };
+            if i['request'] not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target']:
+                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target'][i['request']] = 0
+            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target'][i['request']] += 1
+            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['cnt'] += 1
          
     # update quality: internal server error
     if i['status'] == '500':
