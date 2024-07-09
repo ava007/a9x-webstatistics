9b38b4c6faac6391fd211c3513df91e7f4a8086f
ava007 <3790170+ava007@users.noreply.github.com>
2024-07-09 06:46:44 +0200

    Update updateStatisticsV0001.py

--- src/a9x_webstatistics/updateStatisticsV0001.py
+++ src/a9x_webstatistics/updateStatisticsV0001.py
@@ -85,12 +85,12 @@ def updV0001(
             rdomain = refurl.removeprefix('www.')
             if 'externalFriendsHits' not in d['v0001']['days'][dtFriends]['user']:
                 d['v0001']['days'][dtFriends]['user']['externalFriendsHits'] = {}
-            if owndomain not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits']:
-                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][owndomain] = {'cnt': 0, 'target': {} };
-            if i['request'] not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][owndomain]['target']:
-                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][owndomain]['target'][i['request']] = 0
-            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][owndomain]['target'][i['request']] += 1
-            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][owndomain]['cnt'] += 1
+            if rdomain not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits']:
+                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain] = {'cnt': 0, 'target': {} };
+            if i['request'] not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target']:
+                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target'][i['request']] = 0
+            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target'][i['request']] += 1
+            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['cnt'] += 1
          
     # update statistics for ROBOTS:
     if devCla not in ('desktop','mobile','tablet'):
