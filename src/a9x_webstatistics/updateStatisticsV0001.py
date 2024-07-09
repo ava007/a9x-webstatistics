6f203f414c0d6466777fe01a7c2494306029c090
ava007 <3790170+ava007@users.noreply.github.com>
2024-07-09 15:02:35 +0200

    Update updateStatisticsV0001.py

--- src/a9x_webstatistics/updateStatisticsV0001.py
+++ src/a9x_webstatistics/updateStatisticsV0001.py
@@ -69,28 +69,28 @@ def updV0001(
             d['v0001']['days'][dt]['user']['visits'] = d['v0001']['days'][dt]['user']['visits'] + 1;
             visitIP[i['ip']] = 1
 
-        # top urls: accumulate top urls on the first day of the month
-        dtTopUrl = dt[0:6] + '01'
+        # top urls: accumulate top urls
+        #dtTopUrl = dt[0:6] + '01'
         if i['status'] == '200':
-            if 'topUrl' not in d['v0001']['days'][dtTopUrl]['user']:
-                d['v0001']['days'][dtTopUrl]['user']['topUrl'] = {}
-            if i['request'] not in d['v0001']['days'][dtTopUrl]['user']['topUrl']:
-                d['v0001']['days'][dtTopUrl]['user']['topUrl'][i['request']] = 0;
-            d['v0001']['days'][dtTopUrl]['user']['topUrl'][i['request']] += 1;
+            if 'topUrl' not in d['v0001']['days'][dt]['user']:
+                d['v0001']['days'][dt]['user']['topUrl'] = {}
+            if i['request'] not in d['v0001']['days'][dt]['user']['topUrl']:
+                d['v0001']['days'][dt]['user']['topUrl'][i['request']] = 0;
+            d['v0001']['days'][dt]['user']['topUrl'][i['request']] += 1;
 
         # update friends on the first day of the month:
-        dtFriends = dt[0:6] + '01'
+        #dtFriends = dt[0:6] + '01'
         if i['status'] == '200' and len(i['referer']) > 1 and i['referer'][0:4] == 'http' and owndomain not in i['referer']:
             refurl = urlparse(i['referer']).netloc
             rdomain = refurl.removeprefix('www.')
-            if 'externalFriendsHits' not in d['v0001']['days'][dtFriends]['user']:
-                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'] = {}
-            if rdomain not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits']:
-                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain] = {'cnt': 0, 'target': {} };
-            if i['request'] not in d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target']:
-                d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target'][i['request']] = 0
-            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['target'][i['request']] += 1
-            d['v0001']['days'][dtFriends]['user']['externalFriendsHits'][rdomain]['cnt'] += 1
+            if 'externalFriendsHits' not in d['v0001']['days'][dt]['user']:
+                d['v0001']['days'][dt]['user']['externalFriendsHits'] = {}
+            if rdomain not in d['v0001']['days'][dt]['user']['externalFriendsHits']:
+                d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain] = {'cnt': 0, 'target': {} };
+            if i['request'] not in d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain]['target']:
+                d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain]['target'][i['request']] = 0
+            d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain]['target'][i['request']] += 1
+            d['v0001']['days'][dt]['user']['externalFriendsHits'][rdomain]['cnt'] += 1
          
     # update statistics for ROBOTS:
     if devCla not in ('desktop','mobile','tablet'):
