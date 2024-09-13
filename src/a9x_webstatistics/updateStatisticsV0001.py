026ad9928749e54d78178612fcd09e8be8f59c9e
ava007 <3790170+ava007@users.noreply.github.com>
2024-09-13 19:38:25 +0200

    Update updateStatisticsV0001.py

--- src/a9x_webstatistics/updateStatisticsV0001.py
+++ src/a9x_webstatistics/updateStatisticsV0001.py
@@ -142,7 +142,7 @@ def updV0001(
 
     # accumulate user navigation on first day of the month
     if len(i['referer']) > 1 and (i['status'] == '200') and devCla in ('desktop','mobile','tablet'):
-        dt01 = i['ym'] + '01'
+        dt01 = i['ymd'][0:6] + '01'
         if 'navigation' not in d['v0001']['days'][dt01]:
            d['v0001']['days'][dt01]['navigation'] = {}
         nkey = i['referer'] + '(())' + i['request']
