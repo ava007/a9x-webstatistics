01600e2659fd5ad4b8c719c163ee3379b3c57413
ava007 <3790170+ava007@users.noreply.github.com>
2024-09-23 19:07:54 +0200

    Update test_main000.py

--- tests/test_main000.py
+++ tests/test_main000.py
@@ -1,1 +1,28 @@
+import unittest
+from pathlib import Path
 
+from a9x_webstatistics.main import *
+from a9x_webstatistics.updatestatistics import upd
+from subprocess import run
+
+class TestMain(unittest.TestCase):
+
+    def test_main(self):
+        print("home: " + str(Path.home()) )
+        #cmddata = run('find / -name "Geo*" -print', capture_output=True, shell=True, text=True)
+        #print(cmddata.stdout) 
+        #print(cmddata.stderr) 
+
+        # calling runws expecting return 0
+        assert runws(statfile="webstat.json", infile="nginx_access0.log", geoip="GeoIP2-Country.mmdb", verbosity="0", domain="http://logikfabrik.com") == 0
+        file = Path("webstat.json")  
+        with open(file) as f:  
+            file_data = f.read()  
+        print(str(file_data))
+        contents = json.loads(file_data)
+
+        # check with nginx_access0.log
+        assert '20230923165709' in contents['timelastrec']
+
+if __name__ == '__main__':
+    unittest.main()
