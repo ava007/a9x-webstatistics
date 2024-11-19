59182aaabe5dbac2442cabe2a1bd300a717e48b3
ava007 <3790170+ava007@users.noreply.github.com>
2024-11-19 19:08:29 +0100

    Update main.py

--- src/a9x_webstatistics/main.py
+++ src/a9x_webstatistics/main.py
@@ -57,7 +57,7 @@ def parseRec(rec, log_pattern, j, georeader):
         if country:
             r['country'] = country
     else:
-        print("Log Rec parsing failed for: " + str(data)[0:50])
+        print("Log Rec parsing failed for: " + rec[0:50])
         j['records_parsing_failed'] += 1
         
     return r,j
