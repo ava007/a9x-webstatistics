e456f747bf22a065200b41916ecb951262e4074a
ava007 <3790170+ava007@users.noreply.github.com>
2024-11-30 15:53:30 +0100

    Update parseRecJsonV0001.py

--- src/a9x_webstatistics/parseRecJsonV0001.py
+++ src/a9x_webstatistics/parseRecJsonV0001.py
@@ -1,11 +1,34 @@
 import ast
 #import io, sys, argparse, json, ast
 
-def(rec, lasttimerecobj, geoip):
-  r = ast.literal_eval(rec)    # parse rec to dict
-  dto = datetime.strptime(r['time_local'], "%d/%b/%Y:%H:%M:%S %z")   # 07/Jan/2024:14:06:24 +0000
+def(rec, lasttimerecobj, georeader):
+    r = ast.literal_eval(rec)    # parse rec to dict
+    dto = datetime.strptime(r['time_local'], "%d/%b/%Y:%H:%M:%S %z")   # 07/Jan/2024:14:06:24 +0000
 
-  # skip already processed data:
-  if dto  <=  lasttimerecobj:
-     continue
-  return
+    # skip already processed data:
+    if dto  <=  lasttimerecobj:
+        continue
+
+    if georeader:
+        try:
+            grrsp = georeader.country(r['remote_addr'])
+            country = grrsp.country.name
+        except geoip2.errors.AddressNotFoundError:
+            country = None
+    else:
+        country = None
+        
+    ret = {
+            'ip': r['remote_addr'],
+            'ymd': dto.strftime("%Y%m%d"),
+            'timestamp': dto.strftime("%Y%m%d%H%M%S") ,
+            'request': r['request'],
+            'status': r['status'],
+            'bytes_sent': r['bytes_sent'],
+            'referer': r['referer'],
+            'user_agent': r['user_agent']
+        }
+    if country:
+        r['country'] = country
+
+    return ret
