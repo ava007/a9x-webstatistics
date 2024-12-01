11de805d128d14c43071ee811eb4c48952db1327
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-01 08:43:55 +0100

    Update parseRecJsonV0001.py

--- src/a9x_webstatistics/parseRecJsonV0001.py
+++ src/a9x_webstatistics/parseRecJsonV0001.py
@@ -1,22 +1,13 @@
 import ast
 #import io, sys, argparse, json, ast
 
-def(rec, lasttimerecobj, georeader):
+def(rec, lasttimerecobj):
     r = ast.literal_eval(rec)    # parse rec to dict
     dto = datetime.strptime(r['time_local'], "%d/%b/%Y:%H:%M:%S %z")   # 07/Jan/2024:14:06:24 +0000
 
     # skip already processed data:
     if dto  <=  lasttimerecobj:
-        continue
-
-    if georeader:
-        try:
-            grrsp = georeader.country(r['remote_addr'])
-            country = grrsp.country.name
-        except geoip2.errors.AddressNotFoundError:
-            country = None
-    else:
-        country = None
+        return
         
     ret = {
             'ip': r['remote_addr'],
@@ -28,8 +19,6 @@ def(rec, lasttimerecobj, georeader):
             'referer': r['referer'],
             'user_agent': r['http_user_agent']
         }
-    if country:
-        ret['country'] = country
     if r['upstream_respone_time'] is not None:
         ret['response_time'] = r['upstream_respone_time']
     if r['upstream_cache_status'] is not None:
