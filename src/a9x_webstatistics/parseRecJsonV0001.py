e98fd653dd3757003b8db0ab2a086f29de490125
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-01 19:17:41 +0100

    Update parseRecJsonV0001.py

--- src/a9x_webstatistics/parseRecJsonV0001.py
+++ src/a9x_webstatistics/parseRecJsonV0001.py
@@ -1,7 +1,7 @@
 import ast
 #import io, sys, argparse, json, ast
 
-def(rec, lasttimerecobj):
+def parseRecJsonV0001(rec, lasttimerecobj):
     r = ast.literal_eval(rec)    # parse rec to dict
     dto = datetime.strptime(r['time_local'], "%d/%b/%Y:%H:%M:%S %z")   # 07/Jan/2024:14:06:24 +0000
 
