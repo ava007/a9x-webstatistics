01d26623784a3157a8efd8981ab3ce7ad96827cf
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-01 19:23:39 +0100

    Update parseRecJsonV0001.py

--- src/a9x_webstatistics/parseRecJsonV0001.py
+++ src/a9x_webstatistics/parseRecJsonV0001.py
@@ -1,13 +1,13 @@
 import ast
 #import io, sys, argparse, json, ast
 
-def parseRecJsonV0001(rec, lasttimerecobj):
+def parseRecJsonV0001(rec):
     r = ast.literal_eval(rec)    # parse rec to dict
     dto = datetime.strptime(r['time_local'], "%d/%b/%Y:%H:%M:%S %z")   # 07/Jan/2024:14:06:24 +0000
 
     # skip already processed data:
-    if dto  <=  lasttimerecobj:
-        return
+    #if dto  <=  lasttimerecobj:
+    #    return
         
     ret = {
             'ip': r['remote_addr'],
