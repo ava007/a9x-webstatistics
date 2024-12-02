d2280a83c650c6e5b6acee4151f65b40c430111a
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-02 22:47:57 +0100

    Update parseRecJsonV0001.py

--- src/a9x_webstatistics/parseRecJsonV0001.py
+++ src/a9x_webstatistics/parseRecJsonV0001.py
@@ -4,10 +4,6 @@ from datetime import datetime
 def parseRecJsonV0001(rec):
     r = ast.literal_eval(rec)    # parse rec to dict
     dto = datetime.strptime(r['time_local'], "%d/%b/%Y:%H:%M:%S %z")   # 07/Jan/2024:14:06:24 +0000
-
-    # skip already processed data:
-    #if dto  <=  lasttimerecobj:
-    #    return
         
     ret = {
             'ip': r['remote_addr'],
@@ -16,7 +12,7 @@ def parseRecJsonV0001(rec):
             'request': r['request'],
             'status': r['status'],
             'bytes_sent': r['bytes_sent'],
-            'referer': r['referer'],
+            'referer': r['http_referer'],
             'user_agent': r['http_user_agent']
         }
     if r['upstream_respone_time'] is not None:
