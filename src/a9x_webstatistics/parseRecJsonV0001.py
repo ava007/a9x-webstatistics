197a485f2b545a3d596b5ee57a7a2d92dd6582ea
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-15 17:48:47 +0100

    Update parseRecJsonV0001.py

--- src/a9x_webstatistics/parseRecJsonV0001.py
+++ src/a9x_webstatistics/parseRecJsonV0001.py
@@ -86,6 +86,7 @@ def parseRecJsonV0001(rec):
 import re
 
 # 'da, en-gb;q=0.8, en;q=0.7'
+# 'fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'
 
 def parse_accept_language(accept_language_input):
     if not accept_language_input:
