18e2459cff89c7d2d0c4068c1d68e9698d3753dd
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-16 07:06:51 +0100

    Update updateStatisticsV0001.py

--- src/a9x_webstatistics/updateStatisticsV0001.py
+++ src/a9x_webstatistics/updateStatisticsV0001.py
@@ -242,7 +242,7 @@ def updV0001(
     # en-US,en;q=0.5
     # de,en-US;q=0.7,en;q=0.3
     # zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7
-    if 'accept_language' in i and devCla in ('desktop','mobile','tablet'):
+    if 'accept_language' in i and len(i['accept_language']) > 0 and devCla in ('desktop','mobile','tablet'):
         print(str(i['accept_language']) )
         #if 'language' not in d['v0001']['days'][dt]['user']:
         #    d['v0001']['days'][dt]['user']['language'] = {}
