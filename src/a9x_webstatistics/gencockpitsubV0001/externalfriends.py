bb4ec9e3b4b5f65321237e8c2bf40e707fa66360
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-23 07:42:09 +0100

    Update externalfriends.py

--- src/a9x_webstatistics/gencockpitsubV0001/externalfriends.py
+++ src/a9x_webstatistics/gencockpitsubV0001/externalfriends.py
@@ -19,18 +19,23 @@ def externalFriends(d, owndomain, omit):
                 # check if url is blocked for display:
                 if any(oelm in tk for oelm in omit):  # don not show parts of url 
                     continue
-                if tk not in topland:
-                    topland[tk] = {'cnt': 0, 'target': {} }
+                print("tk: " + str(tk)))
+                if tk not in tland:
+                    tland[tk] = {}
                 for tx in tv.items():
-                topland[tk] = tv
-        if len(topland) > 0:
+                    print("tv: " + str(tx))
+                    if tv.target not in tland[tk][tx]:
+                        tland[tk][tx] = 0
+                    tland[tk][tx] += 1
+                    
+        if len(tland) > 0:
             h  = '<div class="col-md-12 col-lg-6 col-xxl-4">'
             h += '<div class="card mt-2"><div class="card-body">'
             h += '<h3 class="card-title">Top 10 Landings</h3>'
             h += '<p class="card-text">User landings for the current month:</p>'
             h += '<table class="table-responsive text-break"><thead><tr><th scope="col" style="text-align: left">Rank</th><th scope="col">Source</th><th scope="col">Target</th><th scope="col" style="text-align: right">Count</th></tr></thead>'
             i = 1
-            for k, v in sorted(topland.items(), key=itemgetter(0), reverse=True):
+            for k, v in sorted(tland.items(), key=itemgetter(0), reverse=True):
                 # check if url is blocked for display:
                 if any(oelm in k for oelm in omit):  # don not show parts of url 
                      continue
