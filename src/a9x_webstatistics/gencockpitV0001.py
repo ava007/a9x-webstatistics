a9cbe0fd8db542f18fe2a4ac1d5f4aad9e08fbf6
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-23 07:48:29 +0100

    Update gencockpitV0001.py

--- src/a9x_webstatistics/gencockpitV0001.py
+++ src/a9x_webstatistics/gencockpitV0001.py
@@ -304,35 +304,9 @@ def runGenCockpitV0001(infile, outfile, domain, omit):
                 h += '</tr>'
             h += '</table>'
             h += '</div></div></div>'   # end of card and col
-
-        
        
         # top external landings (friends):
-        tland = {}   # nested dictionary!
-        firstOfCurrentMonth =  actYearMonth + '01'
-        if firstOfCurrentMonth in d['v0001']['days'] and 'externalFriendsHits' in d['v0001']['days'][firstOfCurrentMonth]['user']:
-            h += '<div class="col-md-12 col-lg-6 col-xxl-4">'
-            h += '<div class="card mt-2"><div class="card-body">'
-            h += '<h3 class="card-title">Top 10 Landings</h3>'
-            h += '<p class="card-text">User landings for the current month:</p>'
-            h += '<table class="table-responsive text-break"><thead><tr><th scope="col" style="text-align: left">Rank</th><th scope="col">Source</th><th scope="col">Target</th><th scope="col" style="text-align: right">Count</th></tr></thead>'
-            i = 1
-            for k, v in sorted(d['v0001']['days'][firstOfCurrentMonth]['user']['externalFriendsHits'].items(), key=itemgetter(0), reverse=True):
-                # check if url is blocked for display:
-                if any(oelm in k for oelm in omit):  # don not show parts of url 
-                     continue
-                if is_valid_ip(k) == True:  # to suppress ip; ip is not a domain anyway    
-                     continue
-
-                for kb, vb in v['target'].items():
-                    if owndomain not in k:
-                        h += '<tr><td>' + str(i) + '.</td><td>' + k + '</td><td>' + str(kb) + '</td><td style="text-align: right">' + str(vb) + '</td></tr>'
-                        i += 1
-                    if i > 10:
-                        break
-
-            h += '</table>'
-            h += '</div></div></div>'  + "\n"   # end of card and col
+        h += externalFriends(d, owndomain, omit)
 
         tquality = {}   # nested dictionary!
         for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
