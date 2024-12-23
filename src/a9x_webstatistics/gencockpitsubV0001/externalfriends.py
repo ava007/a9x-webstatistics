6213a1525de71601d0515008b84f58a66014e90a
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-23 09:21:32 +0100

    Update externalfriends.py

--- src/a9x_webstatistics/gencockpitsubV0001/externalfriends.py
+++ src/a9x_webstatistics/gencockpitsubV0001/externalfriends.py
@@ -34,7 +34,9 @@ def externalFriends(d, owndomain, omit):
             h += '<div class="card mt-2"><div class="card-body">'
             h += '<h3 class="card-title">Top 10 Landings</h3>'
             h += '<p class="card-text">User landings for the current month:</p>'
-            h += '<table class="table-responsive text-break"><thead><tr><th scope="col" style="text-align: left">Rank</th><th scope="col">Source</th><th scope="col">Target</th><th scope="col" style="text-align: right">Count</th></tr></thead>'
+            h += '<table class="table-responsive text-break"><thead>'
+            h += '<tr><th scope="col" style="text-align: left">Rank</th><th scope="col">Source</th><th scope="col">Target</th><th scope="col" style="text-align: right">Count</th></tr>'
+            h += '</thead>'
             i = 1
             for k, v in sorted(tland.items(), key=itemgetter(0), reverse=True):
                 # check if url is blocked for display:
@@ -51,5 +53,12 @@ def externalFriends(d, owndomain, omit):
                         break
 
             h += '</table>'
-            h += '</div></div></div>'  + "\n"   # end of card and col
+            h += '</div></div></div>'
     return h
+
+def is_valid_ip(address):
+    try: 
+        x = ipaddress.ip_address(address)
+        return True
+    except:
+        return False
