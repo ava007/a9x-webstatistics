a85c94b83da2ccf8c7005658874e9a38658344b0
ava007 <3790170+ava007@users.noreply.github.com>
2024-12-25 11:02:11 +0100

    Create performance.py

---
+++ src/a9x_webstatistics/gencockpitsubV0001/performance.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import ipaddress
+from operator import itemgetter
+
+# performance for the last 31 days:
+def performance(d, owndomain, omit):
+    time_count = 0
+    time_sum = 0
+    cache_miss = 0
+    cache_hit = 0
+    cache_unknown = 0
+    ic = 0
+    for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
+        # dont take months in account:
+        if len(k) <= 6:
+            continue
+        if 'performance' in d['v0001']['days'][k]:
+            if 'response_time' in d['v0001']['days'][k]['performance']:
+                if 'time_count' in d['v0001']['days'][k]['performance']['response_time']:
+                    time_count += d['v0001']['days'][k]['performance']['response_time']['time_count']
+                    time_sum += d['v0001']['days'][k]['performance']['response_time']['time_sum']
+            if 'cache' in d['v0001']['days'][k]['performance']:
+                ic += 1
+                if 'MISS' in d['v0001']['days'][k]['performance']['cache']:
+                    cache_miss += d['v0001']['days'][k]['performance']['cache']['MISS']
+                elif 'HIT' in d['v0001']['days'][k]['performance']['cache']:
+                    cache_hit += d['v0001']['days'][k]['performance']['cache']['HIT']
+                else:
+                    cache_unknown += d['v0001']['days'][k]['performance']['cache']['unknown']
+        if ic >= 31:
+            break
+      
+    h = ''
+    if time_count > 0 or cache_miss > 0:                
+        h += '<div class="col-md-12 col-lg-6 col-xxl-6">'
+        h += '<div class="card mt-2"><div class="card-body">'
+        h += '<h3 class="card-title">Performance for the last ' + str(ic) + ' days</h3>'
+        #h += '<p class="card-text">User hits refering to external domain:</p>'
+        h += '<table class="table">'
+        if time_count > 0:
+            h += '<tr>'
+            h += '<td>Average Response Time for internal processing:</td><td>' + '{:.2}'.format(time_sum/time_count) + ' Seconds</td>'
+            h += '</tr>'
+        if cache_hit > 0:
+            h += '<tr>'
+            h += '<td>Cache Hit Ratio:</td><td>' + "{:.0}".format(cache_hit / (cache_hit+cache_miss+cache_unknown) * 100 ) + '%</td>'
+            h += '</tr>'
+        h += '</table>'
+        h += '</div></div></div>'
+    return h
