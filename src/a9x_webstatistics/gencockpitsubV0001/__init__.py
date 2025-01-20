380fa6b9bad5d5087fcadb96ce0de18f21913e82
ava007 <3790170+ava007@users.noreply.github.com>
2025-01-20 14:48:18 +0100

    Update __init__.py

--- src/a9x_webstatistics/gencockpitsubV0001/__init__.py
+++ src/a9x_webstatistics/gencockpitsubV0001/__init__.py
@@ -2,3 +2,4 @@ from .cockpitlanguages import cockpitLanguages
 from .externalfriends import externalFriends
 from .genheader import genHeader
 from .performance import performance
+from .navchart import navchart
