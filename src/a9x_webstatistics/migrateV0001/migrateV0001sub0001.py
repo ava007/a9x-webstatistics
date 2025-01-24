046b7b4dec1e3073a10cd8d779dbc33fff300c5d
ava007 <3790170+ava007@users.noreply.github.com>
2025-01-24 08:27:55 +0100

    Update migrateV0001sub0001.py

--- src/a9x_webstatistics/migrateV0001/migrateV0001sub0001.py
+++ src/a9x_webstatistics/migrateV0001/migrateV0001sub0001.py
@@ -1,5 +1,21 @@
 import json
 from copy import deepcopy
+from operator import itemgetter
+
+# This function is not yet in operation i.e. being called. It's in preparation!
+# Migrations:
+# 1. User --> ExternalFriendsHits migrate to User --> Nav   (list of dict)
+# 2. User --> Navigation migrate to User --> Nav
+#
+#     nav:[
+#        { 's': 'www.google.com',  # s=source
+#          't': '/',   # t=target
+#          'c': 1      # count
+#          'p': 'e'    # type: e=external, empty: internal
+#        },
+#     ]
+#
+#
 
 def migv0001sub0001(
     statfile
@@ -14,42 +30,46 @@ def migv0001sub0001(
         print("-s json file is not valid")
         return
 
-    # Migrations:
-    # 1. User --> ExternalFriendsHits migrate to User --> Nav   (list of dict)
-    # 2. User --> Navigation migrate to User --> Nav
-
     # save
     tstat = statfile.replace('.json','v0001sub0001before.json')
     with open(tstat, "w") as sf:
        json.dump(d,sf)
 
-    for x in d['days']:
-        if 'user' in d['v0001']['days'][x]:
-            if 'externalFriendsHits' in d['v0001']['days'][x]['user']:
-                if 'nav' not in d['v0001']['days'][x]['user']:
-                    d['v0001']['days'][x]['user']['nav'] = []
-                for tk, tv in d['v0001']['days'][x]['user']['externalFriendsHits'].items():
+    # iterate on days reverse and consolidate nav data on the last day of the month:
+    lastDay = ''
+    for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
+        # change of month:
+        if lastDay is not None and lastDay[0:6] != k[0:6]:
+            lastDay = k
+        if lastDay is None:  # inital call
+            lastDay = k
+        
+        if 'user' in d['v0001']['days'][k]:
+            if 'externalFriendsHits' in d['v0001']['days'][k]['user']:
+                if 'nav' not in d['v0001']['days'][lastDay]['user']:
+                    d['v0001']['days'][lastDay]['user']['nav'] = []
+                for tk, tv in d['v0001']['days'][k]['user']['externalFriendsHits'].items():
                     print("tk: " + str(tk) + " tv: " + str(tv))
                     for tdk,tdv in tv['target'].items():
                         tmprec = {}
                         tmprec['s'] = tk   # Source
                         tmprec['t'] = tdk  # Target
-                        tmprec['y'] = 'e'  # type: e=external source, i=internal source
+                        tmprec['p'] = 'e'  # type: e=external source, i=internal source
                         tmprec['c'] = int(tdv)   # count
                         print("tmprec: " + str(tmprec))
                         # check if source and target already exists:
                         #if next(item for item in d['v0001']['days'][x]['user']['nav'] if item['s'] == tmprec['s] and item['t'] == tmprec['t']):
-                        for e in d['v0001']['days'][x]['user']['nav']:
+                        for e in d['v0001']['days'][lastDay]['user']['nav']:
                             if e['s'] == tmprec['s']
                                     and e['t'] = tmprec['t']:
                                 e['c'] += tmprec['c']
                             else:
-                                d['v0001']['days'][x]['user']['nav'].append(tmprec)
-                        #todo:  delete externalFriendsHits
-            if 'navigation' in d['v0001']['days'][x]['user']:
-                if 'nav' not in d['v0001']['days'][x]['user']:
-                    d['v0001']['days'][x]['user']['nav'] = []
-                for nk, nv in d['v0001']['days'][x]['user']['navigation'].items():
+                                d['v0001']['days'][lastDay]['user']['nav'].append(tmprec)
+                #todo:  delete externalFriendsHits
+            if 'navigation' in d['v0001']['days'][k]['user']:
+                if 'nav' not in d['v0001']['days'][lastDay]['user']:
+                    d['v0001']['days'][lastDay]['user']['nav'] = []
+                for nk, nv in d['v0001']['days'][k]['user']['navigation'].items():
                     print("navigation nk: " + str(nk) + " nv: " + str(nv))
                     n = nk.split('(())') 
                     if n[0] == n[1]:
@@ -57,15 +77,15 @@ def migv0001sub0001(
                     tmprec = {}
                     tmprec['s'] = n[0]    # Source
                     tmprec['t'] = n[1]    # Target
-                    tmprec['y'] = 'i'     # type: e=external source, i=internal source
+                    #tmprec['p'] = 'i'     # type: e=external source, i=internal source
                     tmprec['c'] = int(nv) # count
-                    for e in d['v0001']['days'][x]['user']['nav']:
+                    for e in d['v0001']['days'][k]['user']['nav']:
                         if e['s'] == tmprec['s']
                                 and e['t'] = tmprec['t']:
                             e['c'] += tmprec['c']
                         else:
-                            d['v0001']['days'][x]['user']['nav'].append(tmprec)
-                #todo:  delete externalFriendsHits
+                            d['v0001']['days'][k]['user']['nav'].append(tmprec)
+                #todo:  delete navgation
 
     # write updated statistic file:
     #with open(statfile, "w") as sf:
