fcfbff5692ea23675b8c326cc7583fa0fd5143ae
ava007 <3790170+ava007@users.noreply.github.com>
2025-01-30 07:07:19 +0100

    Update test_z090_gencockpit010.py

--- tests/test_z090_gencockpit010.py
+++ tests/test_z090_gencockpit010.py
@@ -6,9 +6,6 @@ from subprocess import run
 #from a9x_webstatistics.gencockpitV0001 import *
 from a9x_webstatistics.gencockpitsubV0001 import cockpitlanguages
 from a9x_webstatistics.gencockpitV0001 import runGenCockpitV0001
-#import gencockpitV0001
-#from a9x_webstatistics import *
-#from a9x_webstatistics import gencockpit    # includes runGenCockpitV001
 
 class TestGencockpit010(unittest.TestCase):
 
@@ -17,7 +14,7 @@ class TestGencockpit010(unittest.TestCase):
         print(cmddata.stdout) 
         
         # calling runws expecting return 0
-        assert runGenCockpitV0001(infile="webstat.json",outfile="webstat.html",domain="https://logikfabrik.com", omit='', efeatue='') == 0
+        assert runGenCockpitV0001(infile="webstat.json",outfile="webstat.html",domain="https://logikfabrik.com", omit='', efeature='') == 0
         with open('webstat.html') as f:  
             file_data = f.read()  
         print(str(file_data))
