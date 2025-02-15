0a15edcb70a52d0539847c348af35c8a9c71f561
ava007 <3790170+ava007@users.noreply.github.com>
2025-02-15 09:51:19 +0100

    Update cockpitlanguages.py

--- src/a9x_webstatistics/gencockpitsubV0001/cockpitlanguages.py
+++ src/a9x_webstatistics/gencockpitsubV0001/cockpitlanguages.py
@@ -52,6 +52,7 @@ def cockpitLanguages(d, owndomain):
         h += 'const svg = d3.select("svg")'
         h += '  .attr("width", width + margin.left + margin.right)'
         h += '  .attr("height", height + margin.top + margin.bottom)'
+        h += '  .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; height: intrinsic;");'
         h += '  .append("g")'
         h += '  .attr("transform", `translate(${margin.left}, ${margin.top})`);'
 
