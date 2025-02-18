22fc2e277ee8f279883f6521c48ec89bd7b18eea
ava007 <3790170+ava007@users.noreply.github.com>
2025-02-18 19:22:26 +0100

    Update main.py

--- src/a9x_webstatistics/main.py
+++ src/a9x_webstatistics/main.py
@@ -10,8 +10,9 @@ from .summarizemonth import summonth
 from .summarizemonthV0001 import sumMonthV0001
 from .migratev0001 import migv0001
 from .migratev0001 import delv0000
-# from migrateV0001.migrateV0001sub0001 import migv0001sub0001  no module "migrateV001" found
-from .migrateV0001.migrateV0001sub0001 import migv0001sub0001
+# from migrateV0001.migrateV0001sub0001 import migv0001sub0001   # -->  No module named 'migrateV0001'
+#from .migrateV0001.migrateV0001sub0001 import migv0001sub0001   # No module named 'a9x_webstatistics.migrateV0001'
+import migrateV0001.migrateV0001sub0001
 from .parseRecJsonV0001 import parseRecJsonV0001
 from .sumNavDayV0001 import sumNavDayV0001
 from importlib.metadata import version
