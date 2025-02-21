b8114501d37c823cf58dc5a2edfc4d5a3f203817
ava007 <3790170+ava007@users.noreply.github.com>
2025-02-21 18:39:27 +0100

    Update sumNavDayV0001.py

--- src/a9x_webstatistics/sumNavDayV0001.py
+++ src/a9x_webstatistics/sumNavDayV0001.py
@@ -23,5 +23,37 @@ def sumNavDayV0001(d):
 
     if len(tmpNav) > 1:
         d['v0001']['days'][x]['user']['navigation'] = tmpNav
-           
+
+
+    # nav: accumulate nav on the last day of month:
+    lastDay = None
+    for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
+        # consider only days:
+        if len(k) < 8:
+            continue
+        # change of month or first iteration:
+        if (lastDay is None 
+                or lastDay[0:6] != k[0:6]):
+            lastDay = k
+            continue    # skip youngest day
+
+        # prep youngest day:
+        if 'user' not in d['v0001]['days'][lastDay]:
+            d['v0001]['days'][lastDay]['user'] = {}
+        if 'nav' not in d['v0001][lastDay]['days']['user']:
+            d['v0001]['days'][lastDay]['user']['nav'] = {}
+
+        # iterate though previous days
+        if 'nav' in d['v0001']['days'][k]['user']:
+            for e in d['v0001']['days'][k]['user']['nav']:
+                found = False
+                for f in d['v0001']['days'][lastDay]['user']['nav']:            
+                    if (e['s'] == f['s']
+                        and e['t'] == f['t']):
+                        e['c'] += f['c']
+                    found = True
+                    break
+                if found == False:
+                    d['v0001']['days'][lastDay]['user']['nav'].append(e)
+            del d['v0001']['days'][k]['user']['nav']
     return d
