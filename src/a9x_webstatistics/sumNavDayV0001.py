5cbc467e929a71aca0878262e38888570b516918
ava007 <3790170+ava007@users.noreply.github.com>
2025-02-24 07:20:48 +0100

    Update sumNavDayV0001.py

--- src/a9x_webstatistics/sumNavDayV0001.py
+++ src/a9x_webstatistics/sumNavDayV0001.py
@@ -3,6 +3,7 @@ from operator import itemgetter
 
 # accumulate navigation to the last processed day of the month
 def sumNavDayV0001(d):
+    return
 
     # nav: accumulate nav on the last day of month:
     lastDay = None
