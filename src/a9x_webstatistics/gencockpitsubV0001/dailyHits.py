40516f54f4b0d15e6af419d18732bf6a259230fd
ava007 <3790170+ava007@users.noreply.github.com>
2025-03-02 17:52:24 +0100

    Update dailyHits.py

--- src/a9x_webstatistics/gencockpitsubV0001/dailyHits.py
+++ src/a9x_webstatistics/gencockpitsubV0001/dailyHits.py
@@ -1,1 +1,158 @@
+# -*- coding: utf-8 -*-
+from operator import itemgetter
+import ipaddress
 
+# navigation chart in chord-dependency diagram:
+# https://observablehq.com/@d3/chord-dependency-diagram/2
+
+def dailyHitsVisitsChart(d, owndomain, omit):
+    day_lbl = []
+    day_usr_desktop = []
+    day_usr_mobile = []
+    day_usr_tablet = []
+    day_usr_visits = []
+    day_robot_hits = []
+
+    days = 0
+    for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
+        # omit months or years:
+        if len(k) <= 6:
+            continue
+        if days >= 31:
+            break
+        days += 1
+        day_lbl.append(k)
+        if 'desktop' in d['v0001']['days'][k]['user']['deviceHits']:
+            day_usr_desktop.append(d['v0001']['days'][k]['user']['deviceHits']['desktop'])
+        else:
+            day_usr_desktop.append(0)
+        if 'mobile' in d['v0001']['days'][k]['user']['deviceHits']:
+            day_usr_mobile.append(d['v0001']['days'][k]['user']['deviceHits']['mobile'])
+        else:
+            day_usr_mobile.append(0)
+        if 'tablet' in d['v0001']['days'][k]['user']['deviceHits']:
+            day_usr_tablet.append(d['v0001']['days'][k]['user']['deviceHits']['tablet'])
+        else:
+            day_usr_tablet.append(0)
+        if 'robotHits' in d['v0001']['days'][k]['robot']:
+            day_robot_hits.append(d['v0001']['days'][k]['robot']['robotHits'])
+        else:
+            day_robot_hits.append(0)
+        
+    day_lbl.reverse()
+    day_usr_desktop.reverse()
+    day_usr_mobile.reverse()
+    day_usr_tablet.reverse()
+    day_robot_hits.reverse()
+    day_usr_visits.reverse()
+
+    #https://stackoverflow.com/questions/36435384/d3-js-combining-bar-and-line-chart#36435663
+        
+   
+    # d3js horizontal bubble char in case results are available
+    h = "\n\n"
+    h += '<div class="col-md-12 col-lg-12 col-xxl-12">'
+    h += '<div class="card mt-2"><div class="card-body">'
+    h += '<h3 class="card-title">User Navigation Diagram</h3>'
+    h += '<p class="card-text">Chord Chart for the last ' + str(days) + ' days on ' + owndomain + ':</p>'
+    h += '<div id="navchart-chord-container"></div>'
+    h += '<script type="module">' + "\n"
+    h += 'const data = ' + str(data) + ';' + "\n"
+    h += 'function renderChart(data, options = {}) {'
+    h += 'const rect = document.getElementById("navchart-chord-container").getBoundingClientRect();'
+    h += 'const margins = { top: 20, right: 20, bottom: 40, left: 20 };'
+    h += 'const width = Math.round(rect.width) - margins.left - margins.right;'
+    h += 'const height = Math.round(width*0.9);'
+    h += 'const innerRadius = Math.min(width, height) * 0.5 - 90;'
+    h += 'const outerRadius = innerRadius + 10;' + "\n"
+
+    # Compute a dense matrix from the weighted links in data.
+    h += 'const names = d3.sort(d3.union(data.map(d => d.source), data.map(d => d.target)));'
+    h += 'const index = new Map(names.map((name, i) => [name, i]));'
+    h += 'const matrix = Array.from(index, () => new Array(names.length).fill(0));'
+    h += 'for (const {source, target, value} of data) matrix[index.get(source)][index.get(target)] += value;' + "\n"
+
+    h += 'const chord = d3.chordDirected().padAngle(10 / innerRadius)'
+    h += '.sortSubgroups(d3.descending).sortChords(d3.descending);' + "\n"
+
+    h += 'const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);'
+    h += 'const ribbon = d3.ribbonArrow().radius(innerRadius - 1).padAngle(1 / innerRadius);'
+    h += 'const colors = d3.quantize(d3.interpolateRainbow, names.length);' + "\n"
+
+    h += 'const totalWidth = width + margins.left + margins.right;'
+    h += 'const totalHeight = height;' + "\n"
+    h += 'const svg = d3.select("#navchart-chord-container").append("svg")'
+    h += '.attr("id","tt20250225")'
+    h += '.attr("width", width)'
+    h += '.attr("height", height)'
+    h += '.attr("viewBox", [-width / 2, -height / 2, width, height])'
+    h += '.attr("style", "width: 100%; height: auto; font: 10px sans-serif;");'  + "\n"
+    h += 'const chords = chord(matrix);'
+
+    h += 'const group = svg.append("g").selectAll().data(chords.groups).join("g");'
+    h += 'group.append("path").attr("fill", d => colors[d.index]).attr("d", arc);' + "\n"
+
+    h += 'group.append("text")'
+    h += '.each(d => (d.angle = (d.startAngle + d.endAngle) / 2))'
+    h += '.attr("dy", "0.35em")'
+    h += '.attr("transform", d => `'
+    h += 'rotate(${(d.angle * 180 / Math.PI - 90)})'
+    h += 'translate(${outerRadius + 5})'
+    h += '${d.angle > Math.PI ? "rotate(180)" : ""}'
+    h += '`)'
+    h += '.attr("text-anchor", d => d.angle > Math.PI ? "end" : null)'
+    h += '.text(d => names[d.index]);'  + "\n"
+
+    h += 'group.append("title")'
+    h += '.text(d => `${names[d.index]}'
+    h += '${d3.sum(chords, c => (c.source.index === d.index) * c.source.value)} outgoing →'
+    h += '${d3.sum(chords, c => (c.target.index === d.index) * c.source.value)} incoming ←`);' + "\n"
+
+    h += 'svg.append("g")'
+    h += '.attr("fill-opacity", 0.75)'
+    h += '.selectAll()'
+    h += '.data(chords)'
+    h += '.join("path")'
+    h += '.style("mix-blend-mode", "multiply")'
+    h += '.attr("fill", d => colors[d.target.index])'
+    h += '.attr("d", ribbon)'
+    h += '.append("title")'
+    h += '.text(d => `${names[d.source.index]} → ${names[d.target.index]} ${d.source.value}`);'
+    h += '}' + "\n"
+    h += 'renderChart(data, { backgroundColor: "#f8f8f8" });' + "\n"
+    h += "</script>"
+    h += '</div></div></div>' + "\n"
+    return h
+    
+def addLinkChord(data, e, cnt, owndomain):
+    tmplink = {}
+    tmplink['source'] = e['s']
+    if tmplink['source'] == '/' or tmplink['source'] == None or tmplink['source'] == '':
+        tmplink['source'] = owndomain
+                    
+    tmplink['target'] = e['t']
+    if tmplink['target'] == '/' or tmplink['target'] == None or tmplink['target'] == '':
+        tmplink['target'] = owndomain
+                    
+    tmplink['value'] = e['c']
+    if tmplink['source'] == tmplink['target']:
+       return data, cnt
+                
+    duplicate_found = False
+    for li in data:
+        if (li['source'] == tmplink['source']
+            and li['target'] == tmplink['target']):
+            duplicate_found = True
+            li['value'] += tmplink['value']
+            break
+    if duplicate_found == False:
+        data.append(tmplink)
+        cnt += 1
+    return data, cnt
+
+def is_valid_ip(address):
+    try: 
+        x = ipaddress.ip_address(address)
+        return True
+    except:
+        return False
