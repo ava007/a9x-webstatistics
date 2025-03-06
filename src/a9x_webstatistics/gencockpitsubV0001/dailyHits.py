242796895446093bfaf61ddc3d1a4846a721f17c
ava007 <3790170+ava007@users.noreply.github.com>
2025-03-06 17:52:06 +0100

    Update dailyHits.py

--- src/a9x_webstatistics/gencockpitsubV0001/dailyHits.py
+++ src/a9x_webstatistics/gencockpitsubV0001/dailyHits.py
@@ -74,8 +74,8 @@ def dailyHitsVisitsChart(d, owndomain, omit):
     h += '.unknown("#ccc");'  + "\n"
 
     h += 'const visitline = d3.svg.line()'
-	h += '.x(function (d) { return x(d.d) + x.rangeBand()/2; })'
-	h += '.y(function (d) { return y(d.t); });'
+    h += '.x(function (d) { return x(d.d) + x.rangeBand()/2; })'
+    h += '.y(function (d) { return y(d.t); });'
 
     # A function to format the value in the tooltip.
     h += 'const formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en");'
