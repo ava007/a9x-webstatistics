64e2ae81b187466cf4c7ad8fa8058e2b64204fa9
ava007 <3790170+ava007@users.noreply.github.com>
2025-03-15 19:10:20 +0100

    Create test_main030.py

---
+++ tests/test_main030.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+from pathlib import Path
+
+from a9x_webstatistics.main import *
+from a9x_webstatistics.updatestatistics import *
+
+class TestMain010(unittest.TestCase):
+
+    def test_main010(self):
+        # calling runws expecting return 0
+        assert runws(statfile="webstat.json", infile="nginx_access3.log", geoip="GeoIP2-Country.mmdb", verbosity="0", domain="http://logikfabrik.com") == 0
+        file = Path("webstat.json")  
+        with open(file) as f:  
+            file_data = f.read()  
+        print(str(file_data))
+        contents = json.loads(file_data)
+        assert '20250315175907' in contents['timelastrec']
+      
+
+if __name__ == '__main__':
+    unittest.main()
