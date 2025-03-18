51d530117370a47fe467598e89dabea8a5cfef16
ava007 <3790170+ava007@users.noreply.github.com>
2025-03-18 20:25:52 +0100

    Update gencockpitV0001.py

--- src/a9x_webstatistics/gencockpitV0001.py
+++ src/a9x_webstatistics/gencockpitV0001.py
@@ -238,6 +238,7 @@ def runGenCockpitV0001(infile, outfile, domain, omit, efeature):
         if '95' in efeature:
             h += '<div class="row pt-3">'
             h += dailyTopDomains(d, owndomain, omit)
+            h += dailyTopUrl(d, owndomain, omit)
             h += '</div>'
         
 
