ec6288d8960a3449ac9290306b55deabaed14862
ava007 <3790170+ava007@users.noreply.github.com>
2025-03-23 17:30:12 +0100

    Update gencockpitV0001.py

--- src/a9x_webstatistics/gencockpitV0001.py
+++ src/a9x_webstatistics/gencockpitV0001.py
@@ -180,9 +180,12 @@ def runGenCockpitV0001(infile, outfile, domain, omit, efeature):
         h += '<div class="row pt-3">' +externalFriends(d, owndomain, omit) + '</div>'  + "\n"
 
         h += '<h2 id="LongTerm">Long Term statistics for ' + owndomain + '</h2>'
-        h += '<div class="row pt-3">' + monthlyHitsVisitsChart(d, owndomain, omit) + '</div>' + "\n"
-        h += '<div class="row pt-3">' + monthlyTopDomains(d, owndomain, omit) + '</div>' + "\n"
-        h += '<div class="row pt-3">' + monthlyTopUrl(d, owndomain, omit) + '</div>' + "\n"
+        
+        h += '<div class="row pt-3">' + monthlyHitsVisitsChart(d, owndomain, omit) +  "\n"
+        h +=  monthlyTopDomains(d, owndomain, omit) + '</div>' + "\n"
+        
+        h += '<div class="row pt-3">' + monthlyTopUrl(d, owndomain, omit) + "\n"
+        h += monthlyTopCountries(d, owndomain, omit) + '</div>' + "\n"
 
         tquality = {}   # nested dictionary!
         for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
