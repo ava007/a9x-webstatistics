a1b9414381707b1bed10d02f19763c312ca85964
ava007 <3790170+ava007@users.noreply.github.com>
2025-04-17 07:30:57 +0200

    Update gencockpitV0001.py

--- src/a9x_webstatistics/gencockpitV0001.py
+++ src/a9x_webstatistics/gencockpitV0001.py
@@ -15,91 +15,6 @@ def runGenCockpitV0001(infile, outfile, verbosity, domain, omit, efeature):
     
     with open(infile) as json_file:
         d = json.load(json_file) 
-
-        day_lbl = []
-        day_usr_desktop = []
-        day_usr_mobile = []
-        day_usr_tablet = []
-        day_usr_visits = []
-        day_robot_hits = []
-
-        day_usr_i = 0
-        tsource = {}
-        tcountries = {}
-        try:
-            for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
-                # consider only days:
-                if len(k) < 8:
-                    continue
-                # show at max 31 days:
-                if day_usr_i >= 31:
-                    break
-                day_usr_i += 1
-                day_lbl.append(k)
-                if 'desktop' in d['v0001']['days'][k]['user']['deviceHits']:
-                    day_usr_desktop.append(d['v0001']['days'][k]['user']['deviceHits']['desktop'])
-                else:
-                    day_usr_desktop.append(0)
-                if 'mobile' in d['v0001']['days'][k]['user']['deviceHits']:
-                    day_usr_mobile.append(d['v0001']['days'][k]['user']['deviceHits']['mobile'])
-                else:
-                    day_usr_mobile.append(0)
-                if 'tablet' in d['v0001']['days'][k]['user']['deviceHits']:
-                    day_usr_tablet.append(d['v0001']['days'][k]['user']['deviceHits']['tablet'])
-                else:
-                    day_usr_tablet.append(0)
-                if 'robotHits' in d['v0001']['days'][k]['robot']:
-                    day_robot_hits.append(d['v0001']['days'][k]['robot']['robotHits'])
-                else:
-                    day_robot_hits.append(0)
-                                    
-                # visits:
-                if 'visits' in d['v0001']['days'][k]['user']:
-                    day_usr_visits.append(d['v0001']['days'][k]['user']['visits'])
-                else:
-                    day_usr_visits.append(0)
-
-                # top 10 source domains:
-                if 'nav' in d['v0001']['days'][k]['user']:
-                    # sort by count e['c'] desc:
-                    for e in sorted(d['v0001']['days'][k]['user']['nav'], key=lambda x: x['c'], reverse=True):
-                        if 'p' not in e:   # internal links
-                            continue
-                        if e['s'] in omit:
-                            continue
-                        if e['t'] in omit:
-                            continue
-                        if e['s'] not in tsource:
-                            tsource[e['s']] = 0
-                        tsource[e['s']] += e['c']
-                        
-                # top 10 countries:
-                if 'countryHits' in d['v0001']['days'][k]['user']:
-                    for co,cv in d['v0001']['days'][k]['user']['countryHits'].items():
-                        if co not in tcountries:
-                            tcountries[co] = 0
-                        tcountries[co] += cv
-  
-        except KeyError:
-            print('KeyError occured! ' + str(d['v0001']['days'][k]) )
-            raise
-
-        day_lbl.reverse()
-        day_usr_desktop.reverse()
-        day_usr_mobile.reverse()
-        day_usr_tablet.reverse()
-        day_robot_hits.reverse()
-        day_usr_visits.reverse()
-
-        # order for bars:  smallest traffic is "order 1"
-        order_mobile = 1
-        order_tablet = 2
-        order_desktop = 3
-        # swap orders if needed:
-        if sum(day_usr_tablet) < sum(day_usr_mobile):
-            order_tablet, order_mobile = order_mobile, order_tablet
-        if sum(day_usr_desktop) < sum(day_usr_tablet):
-            order_tablet, order_desktop = order_desktop, order_tablet
         
         h = genHeader(owndomain)
         h += "\n" + '<div class="row pt-3"><div class="col-12">'
@@ -112,45 +27,8 @@ def runGenCockpitV0001(infile, outfile, verbosity, domain, omit, efeature):
         h += '<h2 id="hdDay">Daily statistics for ' + owndomain + '</h2>'
         h += "<p><small>Last record included in statistic: <script>document.write(DT2Locale('" + d['timelastrec'] + "'));</script></small></p>"
         h += '</div></div>'
-        h += "\n" + '<div class="row pt-4"><div class="col-12">'
-        h += '<h3>Hits and Visits for the last 31 days</h3>'
-        h += '<div><canvas id="myChart"></canvas></div>'
-        h += '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>'
-        h += '<script>' + "\n" + 'const ctx = document.getElementById(\'myChart\');'
-        h += "const dctx = new Chart(ctx, {"
-        h += " type: 'bar', "  
-        h += " options: { responsive: true, scales: {"
-        h += "   x: { stacked: true }, "
-        h += "   y: { beginAtZero: true, type: 'logarithmic' }"
-        h += "  } }"
-        h += " ,plugins: { subtitle: { display: true, text: 'Hits per Device Class as of ' + DT2Locale('" + d['timelastrec'] + "') } }"
-        h += " ,data: { datasets: [" 
-        h += "     { type: 'line',label: 'Robot Hits', data: " + str(day_robot_hits) + "}" + "\n"
-        h += "    ,{ type: 'line',label: 'User Visits', data: " + str(day_usr_visits)  + " ,backgroundColor: '#ff0000', borderColor: '#ff0000', tension: 0.1}" + "\n"
-        h += "    ,{ type: 'bar', label: 'User Desktop Hits', data: " + str(day_usr_desktop) + ", stack: 's1', backgroundColor: '#42c5f5', order:" + str(order_desktop) + "}" + "\n"
-        h += "    ,{ type: 'bar', label: 'User Mobile Hits',  data: " + str(day_usr_mobile) + ", stack: 's1', backgroundColor: '#42f5aa', order:" + str(order_mobile) + "}" + "\n"
-        h += "    ,{ type: 'bar', label: 'User Tablet Hits', data: " + str(day_usr_tablet) + ", stack: 's1', backgroundColor: '#f5a742', order:" + str(order_tablet) + "}" + "\n"
-        h += "    ],"
-        h += "    labels: " + str(day_lbl)
-        h += " }," + "\n" + "});"
-        h += "var ymax = 0; "
-        h += "var ymin = 99999;"
-        h += "for (i=0; i<5; i++) {" 
-        h += "  var tmax = Math.max.apply(null, dctx.data.datasets[i].data); "
-        h += "  if (tmax > ymax) {  ymax = tmax; } " 
-        h += "  var tmin = Math.min.apply(null, dctx.data.datasets[i].data); "
-        h += "  if (tmin < ymin) {  ymin = tmin; } " 
-        h += "}" + "\n"
-        h += "rm = ymax % 10;"
-        h += "rm = 10 - rm + 10;"
-        h += "dctx.options.scales.y.max = ymax + rm;"
-        h += "dctx.options.scales.y.min = ymin;"
-        h += "dctx.update();"
-        h += '</script>'
-        h += '</div></div>' + "\n\n"  # end of col and row
 
-        if '20' in efeature:
-            h += '<div class="row pt-4">' + dailyHitsVisitsChart(d, owndomain, omit) + '</div>' + "\n"
+        h += '<div class="row pt-4">' + dailyHitsVisitsChart(d, owndomain, omit) + '</div>' + "\n"
 
         h += '<div class="row pt-4">' + navChord(d, owndomain, omit) + '</div>' + "\n"
         
