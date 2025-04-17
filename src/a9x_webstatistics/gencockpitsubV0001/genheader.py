1228b1d9088377e0286c0037ea819ceffb10cfb4
ava007 <3790170+ava007@users.noreply.github.com>
2025-04-17 07:43:38 +0200

    Update genheader.py

--- src/a9x_webstatistics/gencockpitsubV0001/genheader.py
+++ src/a9x_webstatistics/gencockpitsubV0001/genheader.py
@@ -4,7 +4,7 @@ def genHeader(domain):
     h += '<meta name="viewport" content="width=device-width, initial-scale=1">'
     h += '<meta charset="utf-8">'
     h += '<meta name="robots" content="index,follow">'
-    h += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">'
+    h += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">'
     h += '<script src="https://cdn.jsdelivr.net/npm/d3@7"></script>'
     #h += '<script src="https://unpkg.com/vis-network/standalone/umd/vis-network.min.js"></script>'
     h += "\n" + '<script>'
