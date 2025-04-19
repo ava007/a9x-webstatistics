96b9726fa060882c0b75b25e05d4e6287448dd4c
ava007 <3790170+ava007@users.noreply.github.com>
2025-04-19 20:38:18 +0200

    Update __init__.py

--- src/a9x_webstatistics/__init__.py
+++ src/a9x_webstatistics/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "1.6.7"
+__version__ = "1.6.8-beta.1"
 
 from .module1 import *
 from .updatestatistics import upd
 from . import gencockpitsubV0001   # directory
 from . import migV0001   # directory
 from . import cleanupV0001
+from . import sumV0001
