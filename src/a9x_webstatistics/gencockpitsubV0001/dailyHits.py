cfb4266927c5d4aa4d325a6fc679b4c41adffd90
ava007 <3790170+ava007@users.noreply.github.com>
2025-04-20 18:57:11 +0200

    Update dailyHits.py

--- src/a9x_webstatistics/gencockpitsubV0001/dailyHits.py
+++ src/a9x_webstatistics/gencockpitsubV0001/dailyHits.py
@@ -56,7 +56,8 @@ def dailyHitsVisitsChart(d, owndomain, omit):
     h = "\n\n"
     h += '<div class="col-md-12 col-lg-12 col-xxl-12 pt-4">'
     h += '<h3>Daily User Hits and Visits</h3>'
-    h += "<p>User hits and visits on " + owndomain + " from <script>document.write(DT2Locale('" + startPeriod + "'));</script> to <script>document.write(DT2Locale('" + endPeriod + "'));</script>:</p>"
+    h += "<p>User hits and visits on " + owndomain + " from "
+    h += "<script>document.write(DT2Locale('" + str(startPeriod) + "'));</script> to <script>document.write(DT2Locale('" + str(endPeriod) + "'));</script>:</p>"
     h += '<div id="dhvchart-container"></div>' + "\n"
     h += '<script type="module">' + "\n"
     h += 'const sdata = ' + str(sdata) + ';' + "\n"
