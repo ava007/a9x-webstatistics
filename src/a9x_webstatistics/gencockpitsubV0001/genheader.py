2f59b6d145476d9b84896d1117ff44d809d68f4d
ava007 <3790170+ava007@users.noreply.github.com>
2025-07-05 12:25:46 +0200

    Update genheader.py
    updated link to css library

--- src/a9x_webstatistics/gencockpitsubV0001/genheader.py
+++ src/a9x_webstatistics/gencockpitsubV0001/genheader.py
@@ -4,7 +4,7 @@ def genHeader(domain):
     h += '<meta name="viewport" content="width=device-width, initial-scale=1">'
     h += '<meta charset="utf-8">'
     h += '<meta name="robots" content="index,follow">'
-    h += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">'
+    h += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">'
     h += '<script src="https://cdn.jsdelivr.net/npm/d3@7"></script>'
     h += "\n" + '<script>'
     h += 'function DT2Locale(tdt) {'
