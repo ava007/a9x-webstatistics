1241ceaaabb3e41c704f37401b10ef24622c5f67
ava007 <3790170+ava007@users.noreply.github.com>
2026-01-08 13:47:48 +0100

    Fix URL handling in nav_chord_longterm.py

--- src/a9x_webstatistics/gencockpitsubV0001/nav_chord_longterm.py
+++ src/a9x_webstatistics/gencockpitsubV0001/nav_chord_longterm.py
@@ -178,9 +178,9 @@ def navChordLongterm(d, owndomain, omit):
     h += '.text(d => names[d.index])'
     h += '.on("click", function(event, d) {'
     h += 'const url = names[d.index];'
-    h += 'if (url && !url.startsWith('/')) {'
-    h += '    if (!url.startsWith('http://') && !url.startsWith('https://')) {'
-    h += 'url = 'https://' + url;'
+    h += 'if (url && !url.startsWith("/")) {'
+    h += '    if (!url.startsWith("http://") && !url.startsWith("https://")) {'
+    h += 'url = "https://" + url;'
     h += '} }'
     h += 'if (url) window.open(url, "_blank");'
     h += '});' + "\n\n"
