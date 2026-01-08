9e5579e68c89422216bcb119cf62b0b2910e4a25
ava007 <3790170+ava007@users.noreply.github.com>
2026-01-08 13:41:26 +0100

    Clean up navChord.py by removing commented code
    Removed commented-out code for text labels in navChord.py.

--- src/a9x_webstatistics/gencockpitsubV0001/navChord.py
+++ src/a9x_webstatistics/gencockpitsubV0001/navChord.py
@@ -159,16 +159,7 @@ def navChord(d, owndomain, omit):
     h += '.classed("fade", false);'
     h += '});' + "\n"
 
-  
-    #h += 'group.append("text")'
-    #h += '.each(d => (d.angle = (d.startAngle + d.endAngle) / 2))'
-    #h += '.attr("dy", "0.35em")'
-    #h += '.attr("transform", d => `rotate(${(d.angle * 180 / Math.PI - 90)})'
-    #h += 'translate(${outerRadius + 5}) ${d.angle > Math.PI ? "rotate(180)" : ""} `)'
-    #h += '.attr("text-anchor", d => d.angle > Math.PI ? "end" : null)'
-    #h += '.attr("fill", "black")'
-    #h += '.text(d => names[d.index]);'  + "\n"
-
+    # append links to ribbon
     h += 'group.append("a")'
     h += '.attr("xlink:href", d => names[d.index] ) '
     h += '.attr("target", "_blank") '
