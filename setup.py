a90cc8066a76693c5cc677c8d898c7362391e6ec
ava007 <3790170+ava007@users.noreply.github.com>
2026-08-15 07:17:17 +0200

    Update Python version requirements in setup.py

--- setup.py
+++ setup.py
@@ -36,11 +36,12 @@ setuptools.setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=['geoip2'],
     extras_require={
         'dev': ['check-manifest'],
